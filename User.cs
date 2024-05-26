6a2e2f9c954f479f433ecdb5853360a60b40bb86
Krutyaa <139651197+Krutyaa@users.noreply.github.com>
2024-05-26 15:04:19 +0300

    Last commit (for docs). Final

--- User.cs
+++ User.cs
@@ -1,10 +1,25 @@
 namespace Museum
 {
+    /// <summary>
+    /// Класс посетителей
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// id посетителя
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Имя посетителя
+        /// </summary>
         public string? Name { get; set; }
+        /// <summary>
+        /// Фамилия посетителя
+        /// </summary>
         public string? Surname { get; set; }
+        /// <summary>
+        /// Дата визита посетителя
+        /// </summary>
         public string? DateOfVisit { get; set; }
     }
 }
